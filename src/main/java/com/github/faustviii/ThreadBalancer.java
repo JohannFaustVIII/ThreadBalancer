035599d77379418ec56e2652a3db1ba4b712a941
Faust VIII <joh.faustviii@gmail.com>
2021-02-02 21:07:26 +0100

    Added simple constructor for ThreadBalancer and began tests to validate work of constructor.

--- src/main/java/com/github/faustviii/ThreadBalancer.java
+++ src/main/java/com/github/faustviii/ThreadBalancer.java
@@ -1,4 +1,16 @@
 package com.github.faustviii;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ThreadBalancer {
+
+    //TODO: to think?
+    List<Thread> threads;
+
+    public ThreadBalancer(Runnable... runnables) {
+        threads = Arrays.stream(runnables).map(Thread::new).collect(Collectors.toList());
+    }
+
 }
--- src/test/java/com/github/faustviii/TestThreadBalancer.java
+++ src/test/java/com/github/faustviii/TestThreadBalancer.java
@@ -1,4 +1,43 @@
 package com.github.faustviii;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class TestThreadBalancer {
+
+    private ThreadBalancer testedThreadBalancer;
+
+    @Test
+    public void testAddingRunnable(){
+        Runnable runnable1 = createEmptyRunnable();
+        Runnable runnable2 = createEmptyRunnable();
+
+        testedThreadBalancer = new ThreadBalancer(runnable1, runnable2);
+
+        //check if threadBalancer have thread with runnables
+    }
+
+    @Test
+    public void testAddingRunnableMore() {
+        Runnable runnable1 = createEmptyRunnable();
+        Runnable runnable2 = createEmptyRunnable();
+        Runnable runnable3 = createEmptyRunnable();
+        Runnable runnable4 = createEmptyRunnable();
+        Runnable runnable5 = createEmptyRunnable();
+
+        testedThreadBalancer = new ThreadBalancer(runnable1, runnable2, runnable3, runnable4, runnable5);
+
+        List<Thread> threads = testedThreadBalancer.threads;
+        //check if threadBalancer have thread with runnables
+        Assert.assertEquals(5, threads.size());
+        //TODO: to think how get Thread.target variable from Thread class, not to try with reflection
+        Assert.assertEquals(runnable1, threads.get(0));
+    }
+
+    private Runnable createEmptyRunnable() {
+        return () -> {};
+    }
+
 }
